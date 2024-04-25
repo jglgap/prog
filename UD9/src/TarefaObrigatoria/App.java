387fd367d0a1e56a6b44c2358290c6c464081147
a23ricardosr <a23ricardosr@a26eq05.sanclemente.local>
2024-04-25 12:33:29 +0200

    25/4

--- UD9/src/TarefaObrigatoria/App.java
+++ UD9/src/TarefaObrigatoria/App.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.sql.RowSet;
 import javax.sql.rowset.JdbcRowSet;
@@ -14,7 +16,7 @@ import javax.sql.rowset.RowSetProvider;
 public class App implements PodcastInterface {
 
 	List<Podcast> viewAllPodcast = new ArrayList<>();
-	
+
 	public JdbcRowSet openConnectionInSchool() throws SQLException {
 		// conexion en clase
 		try (JdbcRowSet rowSet = RowSetProvider.newFactory().createJdbcRowSet()) {
@@ -36,6 +38,24 @@ public class App implements PodcastInterface {
 
 	}
 
+	private int revisarAutores(Podcast p) throws SQLException {
+		List<Integer> idAut = new ArrayList<>();
+		RowSet rs = openConnectionInSchool();
+		rs.setCommand("select idAutor from Autor");
+		rs.execute();
+		while (rs.next()) {
+			int x;
+			x = rs.getInt("idAutor");
+			idAut.add(x);
+		}
+		if (idAut.contains(p.getAutor().getIdAutor())) {
+			return p.getAutor().getIdAutor();
+		} else {
+			return idAut.get(0);
+		}
+
+	}
+
 	@Override
 	public boolean insertPodcast(Podcast p, RowSet rs) throws SQLException {
 		if (p.revisarPodcastExistenetes()) {
@@ -52,7 +72,7 @@ public class App implements PodcastInterface {
 			rs.updateInt("duracion", p.getDuracion());
 			rs.updateString("periocidad", p.getPeriodicidad());
 			rs.updateString("formato_video", p.getVideoFormat());
-			rs.updateInt("autor", p.getAutor().getIdAutor());
+			rs.updateInt("autor", revisarAutores(p));
 			rs.insertRow();
 			return true;
 		}
@@ -92,58 +112,95 @@ public class App implements PodcastInterface {
 			p.setDuracion(rs.getInt("duracion"));
 			p.setPeriodicidad(rs.getString("periocidad"));
 			p.setVideoFormat(rs.getString("formato_video"));
-			p.setAutor(checkAuthor( rs.getInt("autor")));
+			p.setAutor(checkAuthor(rs.getInt("autor")));
 			viewAllPodcast.add(p);
 		}
 		return viewAllPodcast;
 
 	}
-	
-	private Autor checkAuthor( int idAutor) throws SQLException{
-		try(JdbcRowSet rowSet = RowSetProvider.newFactory().createJdbcRowSet()){
-			rowSet.setUrl("jdbc:mariadb://dbalumnos.sanclemente.local:3314/RASRpodcast_bd");
-			rowSet.setUsername("alumno");
-			rowSet.setPassword("abc123..");
+
+	private Autor checkAuthor(int idAutor) throws SQLException {
+		RowSet rowSet = openConnectionInSchool();
 		rowSet.setCommand("select idAutor, dni, apellidos, nombre from Autor");
 		rowSet.execute();
-		while(rowSet.next()) {
-			if(rowSet.getInt("idAutor") == idAutor ) {
-				return new Autor(rowSet.getInt("idAutor"),rowSet.getString("dni"),rowSet.getString("apellidos"),rowSet.getString("nombre"));
+		while (rowSet.next()) {
+			if (rowSet.getInt("idAutor") == idAutor) {
+				return new Autor(rowSet.getInt("idAutor"), rowSet.getString("dni"), rowSet.getString("apellidos"),
+						rowSet.getString("nombre"));
 			}
-		}}
+		}
+
 		return null;
-		
+
 	}
-	
+
 	@Override
 	public boolean deletePodcast(Podcast p) throws SQLException {
 		String sURL = "jdbc:mariadb://dbalumnos.sanclemente.local:3314/RASRpodcast_bd";
-		try(Connection con = DriverManager.getConnection(sURL, "alumno", "abc123..")){
-		try (PreparedStatement stmt = con.prepareStatement("DELETE from Podcast WHERE idPodcast = ?")) {
-			stmt.setInt(1, p.getIdPodcast());
-			stmt.executeUpdate();
-			return true;
-		}}
+		try (Connection con = DriverManager.getConnection(sURL, "alumno", "abc123..")) {
+			try (PreparedStatement stmt = con.prepareStatement("DELETE from Podcast WHERE idPodcast = ?")) {
+				stmt.setInt(1, p.getIdPodcast());
+				stmt.executeUpdate();
+				return true;
+			}
+		}
 	}
 
 	@Override
-	public Podcast findByPodcastId(int id, RowSet rs) throws SQLException {
+	public void findByPodcastId(int id) throws SQLException {
+		RowSet rs = openConnectionInSchool();
+		// RowSet rs= openConnectionAtHome();
+		rs.setCommand("select idPodcast,titulo,tipo,calidad,duracion,periocidad,formato_video,autor from Podcast");
+		rs.execute();
+		rs.absolute(id);
+		int parar = 0;
+		do {
+			System.out.println("idPodcast: " + rs.getInt("idPodcast"));
+			System.out.println("Titulo: " + rs.getString("titulo"));
+			System.out.println("Tipo: " + rs.getInt("tipo"));
+			System.out.println("calidad: " + rs.getString("calidad"));
+			System.out.println("Duracion: " + rs.getInt("duracion"));
+			System.out.println("Periocidad: " + rs.getString("periocidad"));
+			System.out.println("Formato de video: " + rs.getString("formato_video"));
+			System.out.println("Autor: " + rs.getInt("autor"));
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Elige lo que quieres hacer ahora\n"
+					+ "1-Hacia atras\n"
+					+ "2-Hacia adelante\n"
+					+ "3-Parar operacion");
+			try {
+				int caso=sc.nextInt();
+				switch(caso) {
+				case 1: 
+					rs.previous();
+					break;
+				case 2:
+					rs.next();
+					break;
+				case 3:
+					parar=1;
+					break;
+				}
+			}catch (InputMismatchException e) {
+				e.printStackTrace();
+				
+			}
+		} while (parar == 0);
 
-		return null;
 	}
 
 	public static void main(String[] args) {
 		App a = new App();
-		Podcast p = new Podcast(80, "venezuela", 0, "alta", 4, "mucha","Mp4",new Autor(1,"","",""));
-		//Genero g = new Genero(7,"Peleas");
+		Podcast p = new Podcast(6, "venezuela", 0, "alta", 4, "mucha", "Mp4", new Autor(99, "", "", ""));
+		// Genero g = new Genero(7,"Peleas");
 		try (JdbcRowSet rowSet = a.openConnectionInSchool()) {
-			//a.insertPodcast(p, rowSet);
-			//a.newGenPodcast(g, rowSet);
-			a.deletePodcast(p);
-			a.viewAllPodcast(rowSet);
+			// a.insertPodcast(p, rowSet);
+			// a.newGenPodcast(g, rowSet);
+			// a.deletePodcast(p);
+			// a.viewAllPodcast(rowSet);
+			a.findByPodcastId(1);
 			System.out.println(a.viewAllPodcast);
 		} catch (SQLException e) {
-		
 			e.printStackTrace();
 		}
 	}
--- UD9/src/TarefaObrigatoria/PodcastInterface.java
+++ UD9/src/TarefaObrigatoria/PodcastInterface.java
@@ -8,14 +8,14 @@ import javax.sql.RowSet;
 
 public interface PodcastInterface {
 
-	public boolean insertPodcast (Podcast p, RowSet rs) throws SQLException;
+	public boolean insertPodcast(Podcast p, RowSet rs) throws SQLException;
 
-	public boolean newGenPodcast(Genero g, RowSet rs) throws SQLException; //already functional
+	public boolean newGenPodcast(Genero g, RowSet rs) throws SQLException; // already functional
 
 	public boolean updatePodcast(Podcast p, RowSet rs) throws SQLException;
 
 	public boolean deletePodcast(Podcast p) throws SQLException;
 
-	public Podcast findByPodcastId(int id, RowSet rs) throws SQLException;
+	public void findByPodcastId(int id) throws SQLException;
 
 }
